58464f106db8b16c25c5364d50557467db867a5a
pknguyen2704 <pknguyen2704@gmail.com>
2026-02-10 08:33:48 +0700

    fix: fix compute engine spark in data gate

--- experiments/scripts/platform_setup.sh
+++ experiments/scripts/platform_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Setup platform
-docker network create datagate_net --driver bridge
-docker network create data_platform_net --driver bridge
+docker network create datagate-net --driver bridge
+docker network create data-platform-net --driver bridge
