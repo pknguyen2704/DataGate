69d9167b372fb3f635e538b0ce1c2b72ffe148e4
pknguyen2704 <pknguyen2704@gmail.com>
2026-02-10 07:59:20 +0700

    fix: update compute engine spark in data platform

--- experiments/data_platform/compute_engine/spark/conf/spark-env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This file is sourced when running various Spark programs.
-# It is used to set environment variables for the Spark daemons (Master/Worker).
-
-SPARK_MASTER_OPTS="-Dspark.ui.prometheus.enabled=true -Duser.timezone=Asia/Ho_Chi_Minh"
-SPARK_WORKER_OPTS="-Dspark.ui.prometheus.enabled=true -Duser.timezone=Asia/Ho_Chi_Minh"
