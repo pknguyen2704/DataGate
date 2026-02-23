7b954111297652ab4c3e7cc1f88f2c07e4d58454
pknguyen2704 <pknguyen2704@gmail.com>
2026-02-23 13:47:33 +0700

    fix: update submit spark job

--- experiments/scripts/experiment_submit_spark_jobs.sh
+++ experiments/scripts/experiment_submit_spark_jobs.sh
@@ -1,4 +1,4 @@
 docker exec data_platform_spark_client /opt/spark/bin/spark-submit \
-  --class pknguyen.date_gate.SimpleSpark \
+  --class pknguyen.date_gate.Load_From_DS_To_Bronze \
   --master spark://data-platform-spark-master:7077 \
   /opt/spark/jobs/java/java_spark_jobs-1.0-SNAPSHOT.jar
