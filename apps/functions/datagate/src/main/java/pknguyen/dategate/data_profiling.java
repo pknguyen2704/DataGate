7d34fd647f091456a3db6b03052a3f762f0a465f
pknguyen2704 <pknguyen2704@gmail.com>
2026-02-23 23:02:35 +0700

    fix: update data profiling

--- apps/functions/datagate/src/main/java/pknguyen/dategate/data_profiling.java
+++ apps/functions/datagate/src/main/java/pknguyen/dategate/data_profiling.java
@@ -1,5 +1,7 @@
 package pknguyen.dategate;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class data_profiling {
@@ -9,6 +11,9 @@ public class data_profiling {
                 .getOrCreate();
         try {
             System.out.println("[INFO] Start data profiling");
+            String table = "rest.bronze.yellow_tripdate";
+            Dataset<Row> df = spark.read().format("iceberg").load(table);
+            System.out.println(df.count());
         } finally  {
             spark.stop();
         }
