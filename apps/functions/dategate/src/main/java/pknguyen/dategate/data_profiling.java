1ef652bd03fbf8537cbe6338960d51bec9d854b4
pknguyen2704 <pknguyen2704@gmail.com>
2026-02-23 21:59:56 +0700

    feat: test datagate profiling function

--- apps/functions/dategate/src/main/java/pknguyen/dategate/data_profiling.java
+++ apps/functions/dategate/src/main/java/pknguyen/dategate/data_profiling.java
@@ -1,4 +1,18 @@
 package pknguyen.dategate;
 
+import org.apache.spark.sql.SparkSession;
+
 public class data_profiling {
+    public static void main(String[] args) throws Exception{
+        long jobStart = System.currentTimeMillis();
+        SparkSession spark = SparkSession.builder()
+                        .appName("[DataGate] Data profiling")
+                        .getOrCreate();
+        try {
+            System.out.println("[DataGate] Data profiling function start");
+        } finally {
+            spark.stop();
+        }
+        System.out.println("DataGate Data Profiling job");
+    }
 }
