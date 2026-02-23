09d4daddd158b1c51e5a8915fedc41c7c0a0e414
pknguyen2704 <pknguyen2704@gmail.com>
2026-02-23 22:18:48 +0700

    fix: fix error in data profiling

--- apps/functions/datagate/src/main/java/pknguyen/dategate/data_profiling.java
+++ apps/functions/datagate/src/main/java/pknguyen/dategate/data_profiling.java
@@ -6,8 +6,8 @@ public class data_profiling {
     public static void main(String[] args) throws Exception{
         long jobStart = System.currentTimeMillis();
         SparkSession spark = SparkSession.builder()
-                        .appName("[DataGate] Data profiling")
-                        .getOrCreate();
+                .appName("[DataGate] Data profiling")
+                .getOrCreate();
         try {
             System.out.println("[DataGate] Data profiling function start");
         } finally {
