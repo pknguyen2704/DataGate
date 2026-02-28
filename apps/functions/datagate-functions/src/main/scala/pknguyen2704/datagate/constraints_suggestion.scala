6058cf9b554813f31129de7e32ab2c06186dfe0a
pknguyen2704 <pknguyen2704@gmail.com>
2026-02-28 19:16:22 +0700

    feat: data profiling

---
+++ apps/functions/datagate-functions/src/main/scala/pknguyen2704/datagate/constraints_suggestion.scala
@@ -0,0 +1,5 @@
+package pknguyen2704.datagate
+
+object constraints_suggestion {
+
+}
---
+++ apps/functions/datagate-functions/src/main/scala/pknguyen2704/datagate/data_profiling.scala
@@ -0,0 +1,43 @@
+package pknguyen2704.datagate
+
+import org.apache.spark.sql.SparkSession
+
+object data_profiling {
+  def main(args: Array[String]): Unit = {
+    val JOB_NAME = "[DataGate] Data Profiling"
+    val CATALOG = "rest"
+    val NAMESPACE = "bronze"
+    val TABLE = "green_tripdata"
+    val REST_URI = "http://iceberg-rest:8181"
+    val WAREHOUSE = "s3://lakehouse/"
+
+    // ===== MinIO (S3 compatible) =====
+    val S3_ENDPOINT = "http://minio:9000"
+    val S3_ACCESS_KEY = "admin"
+    val S3_SECRET_KEY = "miniopassword"
+    val AWS_REGION = "us-east-1"
+
+    val spark = SparkSession.builder()
+      .appName(JOB_NAME)
+      .config("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")
+      .config("spark.sql.catalog." + CATALOG, "org.apache.iceberg.spark.SparkCatalog")
+      .config("spark.sql.catalog." + CATALOG + ".type", "rest")
+      .config("spark.sql.catalog." + CATALOG + ".uri", REST_URI)
+      .config("spark.sql.catalog." + CATALOG + ".warehouse", WAREHOUSE)
+      .config("spark.sql.catalog." + CATALOG + ".io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+      .config("spark.sql.catalog." + CATALOG + ".s3.endpoint", S3_ENDPOINT)
+      .config("spark.sql.catalog." + CATALOG + ".s3.path-style-access", "true")
+      .config("spark.sql.catalog." + CATALOG + ".s3.access-key-id", S3_ACCESS_KEY)
+      .config("spark.sql.catalog." + CATALOG + ".s3.secret-access-key", S3_SECRET_KEY)
+      .config("spark.sql.catalog." + CATALOG + ".s3.region", AWS_REGION)
+      .config("spark.sql.catalog." + CATALOG + ".client.region", AWS_REGION)
+      .config("spark.sql.catalog." + CATALOG + ".s3.ssl-enabled", "false")
+      .getOrCreate()
+    try {
+      val df = spark.table(s"$CATALOG.$NAMESPACE.$TABLE")
+      df.show()
+    } finally {
+      spark.stop()
+    }
+  }
+}
